7076f05b6cebcd2d809106a5d5ff803d1d76a5b7
Bob Kline <bkline@rksystems.com>
2023-02-10 18:10:50 -0500

    Add xlsx to allowed file extensions (OCEEBMS-734)
    This was done for the Meeting form at Robin's request. She says
    there have been at least a few occasions where Board members have
    been working in Excel files. I reminded her that the board members
    don't have permission to post documents on this particular form.

--- web/modules/custom/ebms_meeting/src/Form/MeetingForm.php
+++ web/modules/custom/ebms_meeting/src/Form/MeetingForm.php
@@ -252,7 +252,7 @@ class MeetingForm extends FormBase {
           '#description' => 'Supporting files for this meeting.',
           '#upload_location' => 'public://',
           '#upload_validators' => [
-            'file_validate_extensions' => ['pdf rtf doc docx pptx'],
+            'file_validate_extensions' => ['pdf rtf doc docx pptx xlsx'],
           ],
           '#default_value' => $files,
           '#multiple' => TRUE,
