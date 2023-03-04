36cf0906f93ecbd009c2305be43dd198e6c37528
Bob Kline <bkline@rksystems.com>
2023-03-04 08:13:27 -0500

    Use StreamedResponse for articles/import/dates (OCEEBMS-746)
    We have enough articles in the system now that there's not
    enough RAM available for PHP to hold the entire response in
    memory.

--- web/modules/custom/ebms_import/src/Controller/ArticleImportDates.php
+++ web/modules/custom/ebms_import/src/Controller/ArticleImportDates.php
@@ -3,7 +3,7 @@
 namespace Drupal\ebms_import\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 /**
  * List of when each article was last imported, refreshed, or checked.
@@ -13,24 +13,30 @@ class ArticleImportDates extends ControllerBase {
   /**
    * Return a plain-text response.
    */
-  public function list(): Response {
-    $query = \Drupal::database()->select('ebms_article', 'article');
-    $query->fields('article', [
-      'id', 'source_id', 'import_date', 'update_date', 'data_checked'
-    ]);
-    $query->distinct();
-    $query->orderBy('article.id');
-    $results = $query->execute();
-    $ids = [];
-    foreach ($results as $result) {
-      $date = $result->update_date ?: $result->import_date;
-      if (!empty($result->data_checked) && $result->data_checked > $date) {
-        $date = $result->data_checked;
-      }
-      $ids[] = "{$result->id}\t{$result->source_id}\t{$date}";
-    }
-    $response = new Response(implode("\n", $ids) . "\n");
+  public function list(): StreamedResponse {
+    $response = new StreamedResponse();
     $response->headers->set('Content-type', 'text/plain');
+    $response->setCallback(function () {
+      $query = \Drupal::database()->select('ebms_article', 'article');
+      $query->fields('article', [
+        'id', 'source_id', 'import_date', 'update_date', 'data_checked'
+      ]);
+      $query->distinct();
+      $query->orderBy('article.id');
+      $results = $query->execute();
+      ebms_debug_log('streaming rows');
+      $count = 0;
+      foreach ($results as $result) {
+        $date = $result->update_date ?: $result->import_date;
+        if (!empty($result->data_checked) && $result->data_checked > $date) {
+          $date = $result->data_checked;
+        }
+        echo "{$result->id}\t{$result->source_id}\t{$date}\n";
+        flush();
+        $count++;
+      }
+      ebms_debug_log("finished streaming $count rows");
+    });
     return $response;
   }
 
