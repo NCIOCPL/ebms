3b8273903592446d00e5a3b2fbe1ab866de2fefc
Bob Kline <bkline@rksystems.com>
2024-01-26 09:08:05 -0500

    Upgrade to Drupal 10.2.2 (OCEEBMS-795)
    Also upgrade linkit to 6.1.2 and uswds_base to 3.7.0, and
    create menu proxy class.

--- scripts/release-4.3.sh
+++ scripts/release-4.3.sh
@@ -10,13 +10,19 @@ fi
 
 echo Setting locations
 export NCIOCPL=https://api.github.com/repos/NCIOCPL
-export URL=$NCIOCPL/ebms/tarball/harpers-ferry
+export REPO_URL=$NCIOCPL/ebms/tarball/harpers-ferry
 export WORKDIR=/tmp/ebms-4.3
 export SCRIPTS=$WORKDIR/ebms/scripts
 export CONFIG=$WORKDIR/config
 export BASEDIR=/local/drupal/ebms
 export BACKUP=`/bin/date +"/tmp/ebms-backup-%Y%m%d%H%M%S.tgz"`
-export CURL="curl -L -s -k"
+export USWDS_VERSION=3.7.1
+export DOWNLOAD=https://github.com/uswds/uswds/releases/download
+export USWDS_URL=$DOWNLOAD/v$USWDS_VERSION/uswds-uswds-$USWDS_VERSION.tgz
+export THEME=$BASEDIR/web/themes/custom/ebms
+export USWDS_TARFILE=/tmp/uswds-$USWDS_VERSION.tgz
+export TAR=/bin/tar
+export CURL="/bin/curl -L -s -k"
 echo "Base directory is $BASEDIR"
 
 echo Creating a working directory at $WORKDIR
@@ -32,15 +38,15 @@ mkdir $WORKDIR || {
 
 echo Backing up existing files
 cd $BASEDIR
-tar -czf $BACKUP composer.* scheduled web/modules/custom web/themes/custom || {
+$TAR -czf $BACKUP composer.* scheduled web/modules/custom web/themes/custom || {
     echo "Backup to $BACKUP failed"
     exit 1
 }
 
 echo Fetching the branch from GitHub
 cd $WORKDIR
-$CURL $URL | tar -xzf - || {
-    echo fetch $URL failed
+$CURL $REPO_URL | $TAR -xzf - || {
+    echo fetch $REPO_URL failed
     exit 1
 }
 mv NCIOCPL-ebms* ebms || {
@@ -57,7 +63,8 @@ drush state:set system.maintenance_mode 1 || {
 echo Clearing files and directories which will be refreshed from GitHub
 cd $BASEDIR
 rm -rf README.md composer.* scheduled/* web/modules/custom/*
-rm -rf web/themes/templates
+rm -rf web/themes/custom/ebms/templates
+rm -rf web/themes/custom/ebms/package
 
 echo Refreshing those directories
 cd $WORKDIR/ebms
@@ -70,6 +77,14 @@ cp -r web/themes/custom/ebms/templates $BASEDIR/web/themes/custom/ebms/ || {
 }
 cp scheduled/* $BASEDIR/scheduled/ || { echo cp scheduled failed; exit; }
 cp README.md $BASEDIR/ || { echo cp README.md failed; exit; }
+cd $THEME || {
+    echo unable to switch to custom EBMS theme directory
+    exit 1
+}
+$CURL $USWDS_URL | $TAR -xzf - || {
+    echo fetch $USWDS_URL failed
+    exit 1
+}
 
 echo Applying PHP upgrades
 # composer config --no-plugins allow-plugins.drupal/core-project-message true
