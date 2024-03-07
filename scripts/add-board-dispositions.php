cf467cca0cd1f65b1967c7fecaaf8256393ec217
Bob Kline <bkline@rksystems.com>
2024-03-07 14:12:08 -0500

    Script per-board dispositions (OCEEBMS-808)

--- scripts/add-board-dispositions.php
+++ scripts/add-board-dispositions.php
@@ -1,13 +1,24 @@
 <?php
+
 $storage = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
 $query = $storage->getQuery()->accessCheck(FALSE);
 $query->condition('vid', 'dispositions');
-$all_dispositions = $query->execute();
-sort($all_dispositions, SORT_NUMERIC);
-$original_dispositions = array_slice($all_dispositions, 0, 4);
+$term_ids = $query->execute();
+$dispositions = $storage->loadMultiple($term_ids);
+$disposition_map = [];
+foreach ($dispositions as $disposition) {
+  $disposition_map[$disposition->name->value] = $disposition->id();
+}
+$json_path = __DIR__ . '/../testdata/board_dispositions.json';
+$board_dispositions_json = file_get_contents($json_path);
+$board_dispositions = json_decode($board_dispositions_json, TRUE);
 $boards = \Drupal\ebms_board\Entity\Board::loadMultiple();
 foreach ($boards as $board) {
-  $board->set('review_dispositions', $original_dispositions);
+  $disposition_ids = [];
+  foreach ($board_dispositions[$board->name->value] as $disposition_name) {
+    $disposition_ids[] = $disposition_map[$disposition_name];
+  }
+  $board->set('review_dispositions', $disposition_ids);
   $board->save();
 }
 echo 'assigned review dispositions to ' . count($boards) . " boards\n";
--- testdata/boards.php
+++ testdata/boards.php
@@ -5,33 +5,31 @@ require(__DIR__ . '/console-log.php');
 // Find out where the data is.
 $repo_base = getenv('REPO_BASE') ?: '/var/www/ebms';
 
-// Load the maps.
-$json = file_get_contents("$repo_base/testdata/maps.json");
-$maps = json_decode($json, true);
-
-// Get the default review dispositions.
-$dispositions = [];
-foreach ($maps['dispositions'] as $disposition) {
-  $dispositions[] = $disposition;
-}
-
 // Get the full list of dispositions.
 $storage = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
 $query = $storage->getQuery()->accessCheck(FALSE);
 $query->condition('vid', 'dispositions');
-$all_dispositions = $query->execute();
-sort($all_dispositions, SORT_NUMERIC);
+$term_ids = $query->execute();
+$dispositions = $storage->loadMultiple($term_ids);
+$disposition_map = [];
+foreach ($dispositions as $disposition) {
+  $disposition_map[$disposition->name->value] = $disposition->id();
+}
 
+// Load the per-board review dispositions.
+$json_path = __DIR__ . '/../testdata/board_dispositions.json';
+$board_dispositions_json = file_get_contents($json_path);
+$board_dispositions = json_decode($board_dispositions_json, TRUE);
+
+// Create the board entities.
 $json = file_get_contents("$repo_base/testdata/boards.json");
-$boards = json_decode($json, true);
+$boards = json_decode($json, TRUE);
 foreach ($boards as $values) {
-  if ($values['name'] === 'Screening and Prevention') {
-    $values['review_dispositions'] = $all_dispositions;
+  $disposition_ids = [];
+  foreach ($board_dispositions[$values['name']] as $disposition_name) {
+    $disposition_ids[] = $disposition_map[$disposition_name];
   }
-  else {
-    $values['review_dispositions'] = $dispositions;
-  }
-
+  $values['review_dispositions'] = $disposition_ids;
   $board = \Drupal\ebms_board\Entity\Board::create($values);
   $board->save();
 }
