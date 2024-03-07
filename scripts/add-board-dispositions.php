9a9fa788e365cef973758cb4eabc502f444b3b88
Bob Kline <bkline@rksystems.com>
2024-03-07 16:18:54 -0500

    Add new dispositions (OCEEBMS-808)

--- scripts/add-board-dispositions.php
+++ scripts/add-board-dispositions.php
@@ -9,6 +9,17 @@ $disposition_map = [];
 foreach ($dispositions as $disposition) {
   $disposition_map[$disposition->name->value] = $disposition->id();
 }
+$json_path = __DIR__ . '/../testdata/dispositions.json';
+$dispositions_json = file_get_contents($json_path);
+$disposition_values = json_decode($dispositions_json, TRUE);
+foreach ($disposition_values as $values) {
+  if (!array_key_exists($values['name'], $disposition_map)) {
+    unset($values['id']);
+    $term = \Drupal\taxonomy\Entity\Term::create($values);
+    $term->save();
+    $disposition_map[$values['name']] = $term->id();
+  }
+}
 $json_path = __DIR__ . '/../testdata/board_dispositions.json';
 $board_dispositions_json = file_get_contents($json_path);
 $board_dispositions = json_decode($board_dispositions_json, TRUE);
